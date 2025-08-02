c6bb2bf807d7da76611685e44c808e524a677c14
Ana C Barros <anabarros.mcl@gmail.com>
2025-08-02 15:28:59 -0300

    divisao

--- calculadora.py
+++ calculadora.py
@@ -16,6 +16,12 @@ if __name__ == "__main__":
     print(f"2 + 3 = {soma(2, 3)}") 
     print(f"5.5 + 4.5 = {soma(5.5, 4.5)}") 
 
-    print(f'1900111 + 2025111 = {soma(1900111, 2025111)}')  # Adicionando segundo comentário.
-    print(f'70 + 100 = {soma(70, 100)}')
-    print(f'80 + 100 = {soma(80, 100)}') # Adicionando outro comentário.
+    print(f'1900 + 2025 = {soma(1900, 2025)}')
+
+def divisao (a ,b):
+    return a / b
+
+if __name__ == "__main__":
+    print("Teste da função divisao:")
+    print(f"6 / 3 = {divisao(6, 3)}")  #comentario teste
+     
