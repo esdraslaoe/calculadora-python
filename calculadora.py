0fdf95df62a2dbf5c0d2600fa4ee71e4a74146b8
Ana C Barros <anabarros.mcl@gmail.com>
2025-08-02 11:37:52 -0300

    segundo comentario

--- calculadora.py
+++ calculadora.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     print(f"2 + 3 = {soma(2, 3)}") 
     print(f"5.5 + 4.5 = {soma(5.5, 4.5)}") 
 
-    print(f'1900 + 2025 = {soma(1900, 2025)}')  # Adicionando comentário.
+    print(f'1900111 + 2025111 = {soma(1900111, 2025111)}')  # Adicionando segundo comentário.
     print(f'70 + 100 = {soma(70, 100)}')
