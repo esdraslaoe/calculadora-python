bd691c10f09b2d76d7aca300fe21d97b58d9e2c2
esdraslaoe <esdraslaoe@hotmail.com>
2025-08-02 15:28:49 -0300

    teste

--- calculadora.py
+++ calculadora.py
@@ -18,3 +18,5 @@ if __name__ == "__main__":
 
     print(f'1900111 + 2025111 = {soma(1900111, 2025111)}')  # Adicionando segundo comentário.
     print(f'70 + 100 = {soma(70, 100)}')
+
+    print('tudo certo nada errado')
